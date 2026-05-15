ac47e7d942b64f5d53c202c57417bffbfcf45d32
Bryan Veloz <veloz.bryan99@gmail.com>
2026-05-15 14:14:20 -0500

    creacion del ejemplo2

--- ejemplo2.py
+++ ejemplo2.py
@@ -3,14 +3,14 @@
     simple actualizado
 """
 # variable edad, asume el valor ingresado por teclado
-edad = input("Ingrese la edad de la persona: ")
+edad = input("Ingrese la edad de la persona: ") # 27 cadena
 
 # la variable edad, asume un valor de tipo cadena
 # por defecto con la función input
 # Se debe hacer un cambio de tipo de dato para que
 # sea considerado con entero, a través de int
 
-edad = int(edad)
+edad = int(edad) # 27entero
 
 # desde aquí la variable edad es considerada con tipo 
 # de dato entero
@@ -18,13 +18,13 @@ edad = int(edad)
 # Se realiza la comparación en el condional
 # en función del valor ingresado por teclado
 
-if edad >= 18:
+if edad >= 18 and edad <=25:
     # si la condición verdadera, se ejecutan las líneas de
     # código que pertencen al if
-    print("Usted es mayor de edad en Ecuador.")
+    print("Su edad es permitida para ingresar")
 else:
     # si la condición es falsa en el if, se ejecutan las líneas 
     # de código que pertenecen al else
-    print("Usted es menor de edad en Ecuador.")
+    print("Su edad esta fuera de rango")
 
 print("finalizar programa")
